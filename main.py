e717b34ae21d5c511f3e82cf13ea81fc43b6d3e4
olenayakovchuk <olenayakovchuk03@gmail.com>
2026-02-20 11:49:10 +0200

    change main.py2

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from utils import factorial, fibonacci
+from utils import factorial, fibonacci, find_gcd
 
 def main():
     number = input("Введіть число:")
+    number2 = input("Введіть число2:")
     result = factorial(number)
+    result2 = find_gcd(number, number2)
     result1 = fibonacci(number)
     print(f"Факторіал числа {number} дорівнює {result}")
     print(f"{number}-те число Фібоначчі дорівнює {result1}")
+    print(f"нсд чисел {number}, {number2} дорівнює {result2}")
 
 if name == "__main__":
     main()
