c09313c2c97d960e37b271481319851cd26d4476
yelyzavetabutova <lizabutova2015@gmail.com>
2026-02-20 11:15:05 +0200

    added fibonacci

--- utils.py
+++ utils.py
@@ -4,3 +4,15 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
+
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("n має бути невід'ємним")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        a, b = b, a + b
+    return b
