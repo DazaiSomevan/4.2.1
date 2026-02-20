43543808b28d11ced586c1015451a6838ae9b8a1
yelyzavetabutova <lizabutova2015@gmail.com>
2026-02-20 11:27:22 +0200

    first change to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from utils import factorial
+from utils import factorial, fibonacci
 
 def main():
     number = input("Введіть число:")
     result = factorial(number)
+    result1 = fibonacci(number)
     print(f"Факторіал числа {number} дорівнює {result}")
+    print(f"{number}-те число Фібоначчі дорівнює {result1}")
 
 if name == "__main__":
     main()
